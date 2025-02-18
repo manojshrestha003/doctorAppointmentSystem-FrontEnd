4bc05555ad6d1e328763e1effcd93fab93701065
manojshrestha003 <manoj.shrestha2081@gmail.com>
2025-02-18 22:06:30 +0545

    creating api to register user

--- src/Context/AppContext.jsx
+++ src/Context/AppContext.jsx
@@ -1,13 +1,38 @@
-import { createContext } from "react";
-import { doctors } from "../assets/assets";
+import { createContext, useState } from "react";
+
+import axios from 'axios'
+import { useEffect } from "react";
+import {toast} from 'react-toastify'
 
 export  const AppContex =  createContext();
 
 const AppContextProvier = (props)=>{
+    const backendUrl = import.meta.env.VITE_BACKEND_URL
+    const [doctors, setDoctor] = useState([]);
+
     const  value  = {
-        doctors,
+         doctors
 
     }
+    const getDoctorsData = async()=>{
+        try {
+            const {data} = await axios.get(backendUrl + '/api/doctor/list')
+        if(data.success){
+            setDoctor(data.doctors)
+
+        }
+        else{
+            toast.error(data.message)
+        }
+            
+        } catch (error) {
+            toast.error(error.message)
+        }
+        
+    }
+    useEffect(()=>{
+         getDoctorsData()
+    },[])
 
     return (
         <AppContex.Provider value={value}>
